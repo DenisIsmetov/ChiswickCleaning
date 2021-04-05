1915802c6b6ffb7178a5e9b8cf3609101127b932
DenisIsmetov <59542981+DenisIsmetov@users.noreply.github.com>
2021-04-06 02:23:28 +0300

    Add files via upload

---
+++ message.php
@@ -0,0 +1,19 @@
+<?php 
+
+session_start();
+
+if (isset($_POST['submit'])) {
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$subject = "Message";
+
+$formcontent="From:  ".$name." .\n\n ".$message" .\n\n ".$tel;
+$recipient = "office@chiswickclining.uk";
+$mailheader = "From: ".$email;
+
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+header("location:index.html");
+} 
+?>
